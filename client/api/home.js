14b2671372fa5fb4c693522447592d1a0ab04dcc
cixiu <cheng597604817@qq.com>
2018-09-05 17:45:06 +0800

    feat(api): 添加首页需要的api

---
+++ client/api/home.js
@@ -0,0 +1,45 @@
+/**
+ * GET | LIST 获取首页的推荐列表
+ * @param {*} success 请求成功后的回调函数
+ * @param {*} fail 请求失败后的回调函数
+ */
+const getBookList = (pytype = '', booktype = 132, success = () => {}, fail = () => {}) => {
+  return wx.request({
+    method: 'GET',
+    url: 'https://kanmanapi-main.321mh.com/v1/book/getBookByType/',
+    data: {
+      pagesize: 40,
+      page: 1,
+      pytype, // book的拼音
+      booktype, // book的id 132表示男版， 133表示女版
+      platform: 8,
+      platformname: 'android',
+      productname: 'mht',
+    },
+    success,
+    fail
+  });
+};
+
+/**
+ * GET | LIST 获取首页的排行列表
+ * @param {*} success 请求成功后的回调函数
+ * @param {*} fail 请求失败后的回调函数
+ */
+const getRankList = (success = () => {}, fail = () => {}) => {
+  return wx.request({
+    method: 'GET',
+    url: 'https://rankdata-globalapi.321mh.com/app_api/v1/comic/getIndexRankData/',
+    data: {
+      platformname: 'android',
+      productname: 'mht',
+    },
+    success,
+    fail
+  });
+};
+
+module.exports = {
+  getBookList,
+  getRankList
+}
--- client/pages/home/home.js
+++ client/pages/home/home.js
@@ -1,6 +1,9 @@
-//index.js
+const apiHome = require('../../api/home.js');
 //获取应用实例
 const app = getApp();
+
+// 导航栏的高度 42px
+const tagListHeight = 42;
 
 Page({
   data: {
@@ -11,91 +14,108 @@ Page({
       {
         id: 0,
         name: '排行',
-        urlid: 'rank'
+        urlid: 'rank',
       },
       {
         id: 132,
         name: '推荐',
-        urlid: 'recommend'
+        urlid: '',
       },
       {
         id: 137,
         name: '日更',
-        urlid: 'rigeng'
+        urlid: 'rigeng',
       },
       {
         id: 59,
         name: '漫改',
-        urlid: 'mangai'
+        urlid: 'mangai',
       },
       {
         id: 42,
         name: '社会',
-        urlid: 'shehui'
+        urlid: 'shehui',
       },
       {
         id: 72,
         name: '生活',
-        urlid: 'shenghuo'
-      }
+        urlid: 'shenghuo',
+      },
     ],
-    swiperData: {
-      rank: [],
-      recommend: [],
-      rigeng: [],
-      mangai: [],
-      shehui: [],
-      shenghuo: []
-    }
+    rank: [],
+    recommend: {},
+    rigeng: {},
+    mangai: {},
+    shehui: {},
+    shenghuo: {},
   },
   onLoad: function() {
-    this.setLineBottomStyle(this.data.currentIndex)
-  },
-  onReady: function() {
+    this.setLineBottomStyle(this.data.currentIndex);
     wx.getSystemInfo({
       success: (info) => {
-        console.log(info)
         this.setData({
-          swiperHeight: info.windowHeight - 42
-        })
-      }
-    })
+          swiperHeight: info.windowHeight - tagListHeight,
+        });
+        this.getBookList('', 132, 'recommend');
+      },
+    });
   },
   // 点击顶部tag栏
   switchTag: function(e) {
-    const currentIndex = e.currentTarget.dataset.index
-    this.setLineBottomStyle(currentIndex)
+    const currentIndex = e.currentTarget.dataset.index;
+    this.setLineBottomStyle(currentIndex);
   },
   // 滑动swiper，current 改变时会触发 change 事件
   swiperChange: function(e) {
-    const currentIndex = e.detail.current
-    this.setLineBottomStyle(currentIndex)
-    if (currentIndex === 1) {
-      wx.request({
-        method: 'GET',
-        url: 'https://kanmanapi-main.321mh.com/v1/book/getBookByType/',
-        data: {
-          pagesize: 40,
-          page: 1,
-          booktype: 132,
-          platform: 8,
-          platformname: 'android',
-          productname: 'mht'
-        },
-        success: (res) => {
-          console.log(res)
-        }
-      })
+    const currentIndex = e.detail.current;
+    const tag = this.data.tagList[currentIndex];
+    this.setLineBottomStyle(currentIndex);
+    if (currentIndex === 0 && !this.data.rank.length) {
+      this.getRankList();
+    }
+    if (currentIndex === 1 && !this.data.recommend.name) {
+      this.getBookList(tag.urlid, tag.id, 'recommend');
+    }
+    if (currentIndex === 2 && !this.data.rigeng.name) {
+      this.getBookList(tag.urlid, tag.id, 'rigeng');
+    }
+    if (currentIndex === 3 && !this.data.mangai.name) {
+      this.getBookList(tag.urlid, tag.id, 'mangai');
+    }
+    if (currentIndex === 4 && !this.data.shehui.name) {
+      this.getBookList(tag.urlid, tag.id, 'shehui');
+    }
+    if (currentIndex === 5 && !this.data.shenghuo.name) {
+      this.getBookList(tag.urlid, tag.id, 'shenghuo');
     }
   },
   // 控制tag-item的border-bottom的left值
   setLineBottomStyle: function(currentIndex) {
-    wx.createSelectorQuery().selectAll('.tag-item').boundingClientRect((rects) => {
-      const left = rects[currentIndex].left
-      this.setData({
-        currentIndex: currentIndex,
-        lineStyle: `left: ${left}px`
+    wx.createSelectorQuery()
+      .selectAll('.tag-item')
+      .boundingClientRect((rects) => {
+        const left = rects[currentIndex].left;
+        this.setData({
+          currentIndex: currentIndex,
+          lineStyle: `left: ${left}px`,
+        });
       })
-    }).exec()
-  }
+      .exec();
+  },
+  // 获取首页的书籍列表 (推荐， 日更， 漫改， 社会，生活))
+  getBookList: function(pytype = '', booktype = 132, datakey = 'recommend') {
+    apiHome.getBookList(pytype, booktype, (res) => {
+      this.setData({
+        [datakey]: res.data.data,
+      });
+    });
+  },
+  // 获取首页的排行列表
+  getRankList: function() {
+    apiHome.getRankList((res) => {
+      this.setData({
+        rank: res.data.data,
+      });
+    });
+  },
 });
