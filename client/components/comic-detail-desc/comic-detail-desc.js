267f4da037784e71dcde29075646036f98e9b05d
cixiu <cheng597604817@qq.com>
2018-09-14 23:20:14 +0800

    feat(comic-detail-desc): 收藏漫画至本地缓存

--- client/components/comic-detail-desc/comic-detail-desc.js
+++ client/components/comic-detail-desc/comic-detail-desc.js
@@ -3,6 +3,7 @@ const cache = require('../../utils/cache');
 Component({
   data: {
     readComic: {},
+    collected: false,
   },
   properties: {
     comicId: {
@@ -25,6 +26,8 @@ Component({
   pageLifetimes: {
     show: function() {
       const historyReads = cache.loadHistoryRead() || [];
+      const collections = cache.loadCollections() || [];
+      // 查找当前漫画是否在本地缓存中
       const comic = historyReads.find((item) => {
         return item.comic_id === this.properties.comicId;
       });
@@ -33,7 +36,16 @@ Component({
           readComic: comic,
         });
       }
-    }
+      // 在本地缓存中 查找当前漫画是否已经被收藏过了
+      const collectionComic = collections.find((item) => {
+        return item.comic_id === this.data.comicId;
+      });
+      if (collectionComic) {
+        this.setData({
+          collected: true,
+        });
+      }
+    },
   },
   methods: {
     goToChapterRead: function() {
@@ -50,6 +62,38 @@ Component({
       wx.navigateTo({
         url,
       });
+    },
+    // 收藏漫画
+    collectComic: function() {
+      const collections = cache.loadCollections() || [];
+      const comic = collections.find((item) => {
+        return item.comic_id === this.data.comicId;
+      });
+      if (comic) {
+        wx.showToast({
+          title: '您已收藏过这个漫画了哦',
+          icon: 'none',
+        });
+      } else {
+        const comicInfoBody = this.properties.comicInfoBody;
+        const img_url =
+          'https://image.samanlehua.com/mh/{0}.jpg-480x640.jpg.webp';
+        const comicData = {
+          comic_id: this.data.comicId,
+          comic_name: comicInfoBody.comic_name,
+          last_chapter_name: comicInfoBody.last_chapter_name,
+          update_time: comicInfoBody.update_time,
+          comic_img: img_url.replace('{0}', this.data.comicId),
+        };
+        cache.saveComic(comicData);
+        wx.showToast({
+          title: '收藏成功!',
+          icon: 'success',
+        });
+        this.setData({
+          collected: true,
+        });
+      }
     },
   },
 });
--- client/utils/cache.js
+++ client/utils/cache.js
@@ -9,6 +9,10 @@ const HISTORY_READ_KEY = '__history__';
 // 历史阅读最多存储20条数据
 const HISTORY_READ_MAX_LENGTH = 20;
 
+// 收藏的漫画的key
+const COLLECTION_KEY = '__collection__';
+const COLLECTION_MAX_LENGTH = 20;
+
 const insertArray = (arr, val, compare, maxLen) => {
   const index = arr.findIndex(compare);
   // 如果已经存在 则需要更新一下已经存在的数据
@@ -28,18 +32,46 @@ const insertArray = (arr, val, compare, maxLen) => {
 // 将阅读过的漫画存入storage中
 const saveHistoryRead = (comic) => {
   let historyReads = wx.getStorageSync(HISTORY_READ_KEY) || [];
-  insertArray(historyReads, comic, (item) => {
-    return item.comic_id === comic.comic_id
-  }, HISTORY_READ_MAX_LENGTH);
+  insertArray(
+    historyReads,
+    comic,
+    (item) => {
+      return item.comic_id === comic.comic_id;
+    },
+    HISTORY_READ_MAX_LENGTH,
+  );
   wx.setStorageSync(HISTORY_READ_KEY, historyReads);
   return historyReads;
 };
 
+// 读取阅读过的漫画列表
 const loadHistoryRead = () => {
   return wx.getStorageSync(HISTORY_READ_KEY) || [];
-}
+};
+
+// 收藏漫画
+const saveComic = (comic) => {
+  let collections = wx.getStorageSync(COLLECTION_KEY) || [];
+  insertArray(
+    collections,
+    comic,
+    (item) => {
+      return item.comic_id === comic.comic_id;
+    },
+    COLLECTION_MAX_LENGTH,
+  );
+  wx.setStorageSync(COLLECTION_KEY, collections);
+  return collections;
+};
+
+// 读取收藏过的漫画列表
+const loadCollections = () => {
+  return wx.getStorageSync(COLLECTION_KEY) || [];
+};
 
 module.exports = {
   saveHistoryRead,
-  loadHistoryRead
-}
+  loadHistoryRead,
+  saveComic,
+  loadCollections,
+};
