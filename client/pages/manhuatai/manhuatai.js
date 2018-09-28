44e271bc49530d028ab22db12ab8ec2f0b855724
cixiu <cheng597604817@qq.com>
2018-09-28 13:29:34 +0800

    feat(post-list): 抽离帖子列表post-list为可复用组件

--- client/pages/manhuatai/manhuatai.js
+++ client/pages/manhuatai/manhuatai.js
@@ -1,5 +1,5 @@
 const apiManhuatai = require('../../api/manhuatai');
-const wxDiscode = require('../../wxParse/wxDiscode');
+const util = require('../../utils/util');
 
 Page({
   data: {
@@ -8,7 +8,6 @@ Page({
     page: 1,
     loadMore: true,
     postList: [],
-    descList: [],
   },
   onLoad: function() {
     const params = {
@@ -26,33 +25,8 @@ Page({
     };
     this.getPostList(params);
   },
-  // 预览图片
-  previewImage: function(e) {
-    const index = e.currentTarget.dataset.index;
-    const previewIndex = e.currentTarget.dataset.previewIndex;
-    // 将图片转成没有裁切的大图 -- 图片预览的时候使用
-    const previewImages = this.data.postList[index].Images.map((imgItem) => {
-      return imgItem.replace('200x200', 'noresize');
-    });
-    const current = previewImages[previewIndex];
-    wx.previewImage({
-      urls: previewImages,
-      current,
-    });
-  },
-  // 前往帖子详情查看
-  goToPost: function(e) {
-    const item = e.currentTarget.dataset.item;
-    const satelliteId = item.Id;
-    const starId = item.StarId;
-    const userIdentifier = item.UserIdentifier;
-    wx.navigateTo({
-      url: `/pages/post/post?satelliteId=${satelliteId}&starId=${starId}&userIdentifier=${userIdentifier}`,
-    });
-  },
   // 获取热门帖子列表
   getPostList: function(params) {
-    const descArr = [];
     apiManhuatai.getPostList(params, (res) => {
       if (res.data.data.length === 0) {
         this.setData({
@@ -65,7 +39,7 @@ Page({
       const length = this.data.postList.length;
 
       res.data.data.forEach((item, index) => {
-        item.Content = this.parseContent(item.Content);
+        item.Content = util.parseContent(item.Content);
         // 将图片转成200x200的小图  -- 图片展示的时候使用
         const Images = item.Images.replace(
           /@#de<!--IMG#\d+-->@#de\d+:\d+/g,
@@ -83,15 +57,5 @@ Page({
         loading: false,
       });
     });
-  },
-  // 解析content 除去特殊的字符
-  parseContent: function(content) {
-    // 将html标签去掉
-    const htmlReg = /<[^>]+>/g;
-    // 将自定义的emoji去掉
-    const emojiReg = /\[.*?\]|\{emoji\:(馒头仔\/\d+)\}/g;
-    const contentStr = content.replace(htmlReg, '').replace(emojiReg, ' ');
-    // 将特殊符号的HTML源码转成特殊符号
-    return wxDiscode.strDiscode(contentStr).substring(0, 50);
   },
 });
