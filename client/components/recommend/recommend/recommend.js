536447ef070158929af6eae53ec90259a8c4ead6
cixiu <cheng597604817@qq.com>
2018-10-02 18:10:54 +0800

    fix(recommend): 修复了recommend推荐相关组件中的显示bug

--- client/components/recommend/recommend/recommend.js
+++ client/components/recommend/recommend/recommend.js
@@ -1,5 +1,3 @@
-const utils = require('../../../utils/util');
-
 const app = getApp();
 
 Component({
@@ -26,14 +24,33 @@ Component({
     // 将properties中的数据映射到data中，并过滤成需要的格式
     _setRecommendList: function(recommendData) {
       const bookList = recommendData.book;
-      const jumpData = bookList[1];
+      let jumpData = {};
+      let recommendNew = {};
+      let recommendAuthor = {};
+      let recommendGood = {};
+      bookList.forEach((item) => {
+        if (item.title === '漫画台跳转') {
+          jumpData = item;
+        }
+        if (item.title === '登台新作') {
+          recommendNew = item;
+        }
+        if (item.title === '台长推荐') {
+          recommendAuthor = item;
+        }
+        if (item.title === '频道佳作') {
+          recommendGood = item;
+        }
+      });
       if (!/^\//.test(jumpData.comic_info[0].img_url)) {
         jumpData.comic_info[0].img_url = '/' + jumpData.comic_info[0].img_url;
       }
       this.setData({
         loading: false,
         jumpData,
-        recommendHasMoreList: bookList.slice(3, 5),
+        recommendNew,
+        recommendAuthor,
+        recommendGood,
         recommendNoMoreList: bookList.slice(5),
       });
     },
