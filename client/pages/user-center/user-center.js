ca6bfdfd17782821ed00b5354187dce449dd46d5
cixiu <cheng597604817@qq.com>
2018-10-13 23:18:49 +0800

    feat(user-center): 添加用户个人资料页面

--- client/pages/user-center/user-center.js
+++ client/pages/user-center/user-center.js
@@ -1,5 +1,26 @@
+const filter = require('../../utils/filter');
+
+const app = getApp();
+
 /*
   个人资料的每次更改后，都需要重新拉取用户的信息并且存入storage中
 */
 
-Page({});
+Page({
+  data: {
+    Uavatar: '',
+    userInfo: {},
+  },
+  onLoad: function() {
+    const userInfo = app.globalData.comicUserInfo;
+    const id = userInfo.Uid;
+    const imgHost = 'https://image.samanlehua.com/file/kanmanhua_images/head/';
+    // 生成用户的头像的url
+    const Uavatar = filter.makeImgUrlById(id, imgHost, 'l1x1');
+
+    this.setData({
+      Uavatar,
+      userInfo,
+    });
+  },
+});
